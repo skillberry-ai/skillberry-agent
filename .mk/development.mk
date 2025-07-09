06fa828732d589c7497ae34a13e9005aee6d96e1
AVI WEIT <WEIT@il.ibm.com>
2025-07-09 17:54:24 +0300

    Release uses proper SDK package. (#7)
    * Release uses proper SDK package.
    
    * If sed not installed fail.

--- .mk/development.mk
+++ .mk/development.mk
@@ -38,11 +38,29 @@ release: check_rits_key check-git-main check-git-clean install_requirements  ## 
 		echo "++++++++++++++++++++++++++++++++++++++++++++"; \
 	exit 1; fi
 
+	@command -v sed >/dev/null 2>&1 || { echo "❌ 'sed' is not installed. Aborting."; exit 1; }
 	@echo "++++++++++++++++++++++++++++++++++++++++++++"
 	@echo "=> Creating release with version: $(RELEASE_VERSION)"
 	@echo "++++++++++++++++++++++++++++++++++++++++++++"
 	@sleep 10
 	@echo "===> Generating git tag $(RELEASE_VERSION) and creating GitHub release"
+
+	@git checkout -b branch-$(RELEASE_VERSION)
+	@echo "===> Generated release branch $(RELEASE_VERSION)"
+	sed -i "s|git+ssh://git@github.ibm.com/Blueberry/blueberry-sdk.git#subdirectory=blueberry_tools_service_sdk|git+ssh://git@github.ibm.com/Blueberry/blueberry-sdk.git@$$RELEASE_VERSION#subdirectory=blueberry_tools_service_sdk|" requirements.txt 
+	sed -i "s|git+ssh://git@github.ibm.com/Blueberry/blueberry-sdk.git#subdirectory=blueberry_tools_maker_sdk|git+ssh://git@github.ibm.com/Blueberry/blueberry-sdk.git@$$RELEASE_VERSION#subdirectory=blueberry_tools_maker_sdk|" requirements.txt 
+	git add requirements.txt && \
+	if git diff --cached --quiet; then \
+	  		echo "!!! No updates to commit in blueberry-tools-agent !!!"; \
+	else \
+		echo "!!! Updates detected in blueberry-tools-agent, committing... !!!"; \
+		git config --get user.name >/dev/null || git config user.name "Blueberry CI process" && \
+		git config --get user.email >/dev/null || git config user.email "blueberry.ci@blueberry.ai" && \
+		git commit -m "Update tools_agent requirements file with $(RELEASE_VERSION)" && \
+		git push origin branch-$(RELEASE_VERSION) && \
+		echo "Pushed updated requirements file to blueberry-tools-agent repository (origin branch-$(RELEASE_VERSION))"; \
+	fi
+
 	@git tag -a $(RELEASE_VERSION) -m "Release $(RELEASE_VERSION)" && \
 	git push origin $(RELEASE_VERSION) && \
 	gh release create $(RELEASE_VERSION) --generate-notes
@@ -52,3 +70,4 @@ release: check_rits_key check-git-main check-git-clean install_requirements  ## 
 	@echo "++++++++++++++++++++++++++++++++++++++++++++"
 	@echo "=> Release $(RELEASE_VERSION) created successfully"
 	@echo "++++++++++++++++++++++++++++++++++++++++++++"
+
