30f51fec7118adb5c313b6fc4137c73ca11cc733
AVI WEIT <WEIT@il.ibm.com>
2025-08-07 12:51:44 +0300

    Fix BTA release process. (#11)
    * Fix BTA release process.
    
    * Update gh release create.

--- .mk/development.mk
+++ .mk/development.mk
@@ -61,9 +61,36 @@ release: check_rits_key check-git-main check-git-clean install_requirements  ## 
 		echo "Pushed updated requirements file to blueberry-tools-agent repository (origin branch-$(RELEASE_VERSION))"; \
 	fi
 
+
 	@git tag -a $(RELEASE_VERSION) -m "Release $(RELEASE_VERSION)" && \
-	git push origin $(RELEASE_VERSION) && \
-	gh release create $(RELEASE_VERSION) --generate-notes
+	git push origin $(RELEASE_VERSION)
+
+	#
+	# Important: change to main so that later invocation of "update_git_version" properly works,
+	# Note: update_git_version is called on different contexts later in this flow
+	#
+	@git checkout main
+
+	#
+	# The following block calls either to "basic" gh release command or an "explicit" one:
+	# 
+	# If no previous release exists then "basic" is called
+	# If a previous release exists then "explicit" using commit range is called
+	#
+
+	@REL_PREV_RELEASE=$$(git branch -r | grep 'branch-' | sed 's|.*/branch-||' | sort -V | tail -n 2 | head -n 1); \
+	if [ -z "$$REL_PREV_RELEASE" ] || [ "$$REL_PREV_RELEASE" = "$(RELEASE_VERSION)" ]; then \
+		echo "No previous release found. Creating release with generated notes..."; \
+		gh release create $(RELEASE_VERSION) --generate-notes; \
+	else \
+		echo "Previous release found: $$REL_PREV_RELEASE"; \
+		REL_CURRENT_COMMIT=$$(git rev-parse --short=7 HEAD); \
+		REL_PREV_COMMIT=$$(git merge-base origin/main origin/branch-$$REL_PREV_RELEASE); \
+		echo "Creating release from $$REL_PREV_COMMIT to $$REL_CURRENT_COMMIT..."; \
+		gh release create $(RELEASE_VERSION) --title "$(RELEASE_VERSION)" --notes "$$(git log --pretty=format:'- %s by %an' $$REL_PREV_COMMIT..$$REL_CURRENT_COMMIT)"; \
+	fi
+
+
 
 	@echo "===> Building and pushing new docker image"
 	@make docker_push
